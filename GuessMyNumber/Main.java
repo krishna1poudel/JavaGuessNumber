2753ba6ed5ae071a35af51c9d4f46ee40c985f56
krishna poudel <94549890+krishna1poudel@users.noreply.github.com>
2024-01-05 19:32:41 +0545

    Add files via upload

---
+++ GuessMyNumber/Main.java
@@ -0,0 +1,16 @@
+package GuessMyNumber;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        GuessNumber GN = new GuessNumber();
+        GN.getNumber();
+    }
+}
+
+
+
